9611cb9fb368b6da5f0d1a379a8bde4ab3a6c509
thadar <thadarshinhtet2019@gmail.com>
2025-01-06 10:15:06 +0630

    fix map2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+import Map2 from './Map2';
 import Memory from './Memory';
+import Map from './Map'
+
 
 
 
 function App() {
   return (
-    <Memory/>
+  <Map2/>
   )
 
 }
